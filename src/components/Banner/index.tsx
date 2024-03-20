fe5797447484d1687f54d68ac301b3d662a5170c
Scott Fryer <60462088+steelhead31@users.noreply.github.com>
2024-03-20 08:47:24 +0000

    Update Timestamp

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -8,7 +8,7 @@ const Banner = () => {
 
     return (
       <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
-      <strong className='p-1'>13th October 2023:</strong>
+      <strong className='p-1'>20th March 2024:</strong>
           We are creating the March 2024 PSU binaries for Eclipse Temurin 22.0.0<br/>
           You can track progress <a className='alert-link p-1 text-white' href="https://github.com/adoptium/temurin/issues/35">by platform</a>.
        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
